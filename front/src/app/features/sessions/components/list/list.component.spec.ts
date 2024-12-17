37581c3d818aeeaad3499ce5a18ea5865e5c636d
Lou Mauriege <lou.mauriege@laposte.net>
2024-12-17 11:44:40 +0100

    test(ListComponent): add integration test

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -4,8 +4,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ListComponent } from './list.component';
+import { By } from '@angular/platform-browser';
+import { Session } from '../../interfaces/session.interface';
+import { of } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { SessionApiService } from '../../services/session-api.service';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -17,13 +23,47 @@ describe('ListComponent', () => {
     }
   }
 
+  const mockSessionApiService = {
+    all: jest.fn()
+  };
+
+  const mockSessions: Session[] = [
+    {
+      id: 1,
+      name: 'Yoga Morning',
+      description: 'A refreshing yoga session to start the day.',
+      date: new Date(),
+      teacher_id: 1,
+      users: [1, 2, 3],
+      createdAt: new Date()
+    },
+    {
+      id: 2,
+      name: 'Yoga Afternoon',
+      description: 'A yoga session to end the day.',
+      date: new Date(),
+      teacher_id: 1,
+      users: [4, 5, 6],
+      createdAt: new Date()
+    }
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{ provide: SessionService, useValue: mockSessionService }]
-    })
-      .compileComponents();
+      imports: [
+        HttpClientModule, 
+        MatCardModule, 
+        MatIconModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: SessionApiService, useValue: mockSessionApiService },
+        { provide: SessionService, useValue: mockSessionService }
+      ]
+    }).compileComponents();
+
+    mockSessionApiService.all.mockReturnValue(of(mockSessions));
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
@@ -34,7 +74,49 @@ describe('ListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display the create button if user is admin', () => {
+  it('should display the correct number of sessions', () => {
+    const sessionCards = fixture.debugElement.queryAll(By.css('.item'));
+    expect(sessionCards.length).toBe(mockSessions.length);
+  });
 
+  it('should render the correct title for the first session', () => {
+    const sessionCards = fixture.debugElement.queryAll(By.css('.item'));
+
+    const firstSessionTitle = sessionCards[0].query(By.css('mat-card-title'));
+    expect(firstSessionTitle.nativeElement.textContent).toContain('Yoga Morning');
+  });
+
+  it('should display the create button if user is admin', () => {
+    const createButton = fixture.debugElement.query(By.css('[data-testid="create"]'));
+
+    expect(createButton).toBeTruthy();
+    expect(createButton.nativeElement.textContent).toContain('Create');
+  });
+
+  it('should display the edit button if user is admin', () => {
+    const editButton = fixture.debugElement.queryAll(By.css('[data-testid="edit"]'));
+
+    expect(editButton.length).toBe(2);
+    expect(editButton[0].nativeElement.textContent).toContain('Edit');
+  });
+
+  it('should not display the create button if user is not admin', () => {
+    mockSessionService.sessionInformation = { admin: false };
+    
+    fixture.detectChanges();
+
+    const createButton = fixture.debugElement.query(By.css('[data-testid="create"]'));
+
+    expect(createButton).toBeFalsy();
+  });
+
+  it('should not displat the edit button if user is not admin', () => {
+    mockSessionService.sessionInformation = { admin: false };
+    
+    fixture.detectChanges();
+
+    const editButton = fixture.debugElement.query(By.css('[data-testid="edit"]'));
+
+    expect(editButton).toBeFalsy();
   });
 });
